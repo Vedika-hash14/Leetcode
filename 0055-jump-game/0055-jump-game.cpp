c1a2a41b95bfc947846bfb6dd04a9a0dd57763c1
Vedika-hash14 <vedika.pithadia152978@marwadiuniversity.ac.in>
2026-09-20 12:03:29 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.04%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int reach = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (i > reach)
+                return false;
+
+            if (i + nums[i] > reach)
+                reach = i + nums[i];
+        }
+
+        return true;
+    }
+};
