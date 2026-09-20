67ceab032991142e00a5ebd0974f0b4ebb83fccf
Vedika-hash14 <vedika.pithadia152978@marwadiuniversity.ac.in>
2026-09-20 12:29:56 +0530

    Time: 6 ms (33.69%), Space: 33.1 MB (36.6%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head)
+            return nullptr;
+
+        if (!head->next)
+            return new TreeNode(head->val);
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        prev->next = nullptr;
+
+        TreeNode* root = new TreeNode(slow->val);
+
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+
+        return root;
+    }
+};
